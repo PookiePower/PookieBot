d4b83b537d09df4f79d47f78d2c9d2516e481940
Syudagye <44324659+Syudagye@users.noreply.github.com>
2019-05-18 17:39:22 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (msg, agrs) => {
     .setDescription('Le prefix que vous devez mettre devant les commandes est `' + config.prefix + '`')
     .addField('__Commandes Admin :__', '`helpstaff` - Commandes pour le staff', true)
     .addField('__Commandes Membres :__', '`pookieyt` - La chaine de Pookie, notre maitre a tous ! \n `calinou` - Pas besoin de vous faire un dessin je pense \n `credits` - :thinking: ', true)
-    .addField('__AH et aussi :__', 'Faite moi part des ajouts que vous voudiez voir sur ce bot mar mp (<@464119931282391061>) et aussi des bugs que vous trouvez ^^ !', true)
+    .addField('__AH et aussi :__', 'Faite moi part des ajouts que vous voudiez voir sur ce bot et aussi des bugs que vous trouvez par mp (<@464119931282391061>) ou sur GitHub (https://github.com/Syudagye/PookieBot/issues) !', true)
     )
 }
 
